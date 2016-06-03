7ab2929217ae030932a371d52f506e0c57ff98e9
James Walrath <jwalrath@ucsd.edu>
2016-06-03 07:06:57 -0700

    asdljalds

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,30 +2,37 @@ var critinderApp = angular.module('critinderApp', ['ngRoute', 'ngMaterial', 'ngA
 
 critinderApp.config(function($routeProvider) {
   $routeProvider
-  // home.html route
-  .when('/', {
-    templateUrl : '/static/templates/home.html',
-    controller  : 'HomeController'
-  })
-  .when('/home', {
-    templateUrl : '/static/templates/home.html',
-    controller  : 'HomeController'
-  })
-  .when('/test', {
-      templateUrl : '/static/templates/test.html',
-      controller  : 'HomeController'
-    })
-  .when('/giveCritique', {
-    templateUrl : '/static/templates/give.html',
-    controller  : 'CritiqueProviderController'
-  })
-  .when('/getCritique', {
-    templateUrl : '/static/templates/get.html',
-    controller  : 'CritiqueSeekerController'
-  })
-  .when('/about', {
-    templateUrl : '/static/templates/about.html',
-    controller  : 'AboutController'
-  })
+      .when('/', {
+        templateUrl : '/static/templates/home.html',
+        controller  : 'HomeController'
+      })
+      .when('/home', {
+        templateUrl : '/static/templates/home.html',
+        controller  : 'HomeController'
+      })
+      .when('/test', {
+        templateUrl : '/static/templates/test.html',
+        controller  : 'HomeController'
+      })
+      .when('/giveCritique', {
+        templateUrl : '/static/templates/give.html',
+        controller  : 'CritiqueProviderController'
+      })
+      .when('/giveCritique2', {
+        templateUrl : '/static/templates/give2.html',
+        controller  : 'CritiqueProviderController'
+      })
+      .when('/giveCritique3', {
+        templateUrl : '/static/templates/give3.html',
+        controller  : 'CritiqueProviderController'
+      })
+      .when('/getCritique', {
+        templateUrl : '/static/templates/get.html',
+        controller  : 'CritiqueSeekerController'
+      })
+      .when('/about', {
+        templateUrl : '/static/templates/about.html',
+        controller  : 'AboutController'
+      })
 
 });
