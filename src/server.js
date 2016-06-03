ccaf8f8fa2f80c4ba3c604fec09af6d7b62489af
James Walrath <jwalrath@ucsd.edu>
2016-06-03 01:22:48 -0700

    Misc2

--- src/server.js
+++ src/server.js
@@ -32,6 +32,7 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+<<<<<<< Updated upstream
 app.listen(port, function() {
     console.log('listening on port ' + port);
 });
@@ -54,3 +55,8 @@ app.post('/api/test', function(req, res){
 Mongo Crud Stuff
  */
 var Test = require('./static/model/testModel');
+=======
+app.listen(8000, function() {
+    console.log('listening on port 8000');
+});
+>>>>>>> Stashed changes
